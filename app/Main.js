8032925ee226450c9727a9f2d2eef29da7b90a43
Jose Leal <lealxh@gmail.com>
2021-12-07 20:52:12 -0300

    Initial

---
+++ app/Main.js
@@ -0,0 +1,36 @@
+import React from "react"
+import ReactDOM from "react-dom"
+
+// My Components
+import Header from "./components/Header"
+import HomeGuest from "./components/HomeGuest"
+import Footer from "./components/Footer"
+import About from "./components/About"
+import Terms from "./components/Terms"
+import { BrowserRouter, Switch, Route } from "react-router-dom"
+
+function Main() {
+  return (
+    <BrowserRouter>
+      <Header />
+      <Switch>
+        <Route path="/" exact>
+          <HomeGuest />
+        </Route>
+        <Route path="/about-us">
+          <About />
+        </Route>
+        <Route path="/terms">
+          <Terms />
+        </Route>
+      </Switch>
+      <Footer />
+    </BrowserRouter>
+  )
+}
+
+ReactDOM.render(<Main />, document.querySelector("#app"))
+
+if (module.hot) {
+  module.hot.accept()
+}
